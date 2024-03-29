aba984dab97d8b1e3fb4d27ff697eab68041cfe8
EHAB3x <ehabmah6969@gmail.com>
2024-03-29 14:10:17 +0200

    finish search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,17 @@
-
+import pic from "../assets/profile.jpg"
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="search">
+      <form className="searchForm">
+        <input type="text" placeholder="Find a user"/>
+      </form>
+      <div className="userChat">
+        <img src={pic} alt="pic" />
+        <div className="userChatInfo">
+          <span>Amr</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar"
+import Search from "./Search"
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
         <Navbar />
+        <Search />
     </aside>
   )
 }
