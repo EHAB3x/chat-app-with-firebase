99a1266c3fbb2571314e997cd792d674a43912b9
EHAB3x <ehabmah6969@gmail.com>
2024-04-02 12:33:53 +0200

    add spinner in register btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import ProfilePic from "../assets/profile.jpg"
 import {signOut} from "firebase/auth"
 import { auth } from "../firebase"
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext);
   return (
     <div className="navbar">
       <span className="logo">Ihab chat</span>
       <div className="user">
-        <img src={ProfilePic} alt="Profile Pic" />
-        <span>Ihab</span>
+        <img src={currentUser.photoURL} alt="Profile Pic" />
+        <span>{currentUser.displayName}</span>
         <button onClick={()=> signOut(auth)}>Logout</button>
       </div>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import addAvatar from "../assets/addAvatar.png"
+import addAvatar from "../assets/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
@@ -8,69 +8,77 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
     const [err, setErr] = useState(false);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
-    const handleSubmit = async(e)=>{
-        e.preventDefault();
-        const displayName = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
-        const file = e.target[3].files[0];
-        try{
-            const res = await createUserWithEmailAndPassword(auth, email, password);
-            const storageRef = ref(storage, displayName);
-
-            const uploadTask = uploadBytesResumable(storageRef, file);
-            uploadTask.on(
-            () => {
-                setErr(true);
-            }, 
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-                    await updateProfile(res.user,{
-                        displayName,
-                        photoURL: downloadURL
-                    });
-
-                    await setDoc(doc(db,"users",res.user.uid),{
-                        uid: res.user.uid,
-                        displayName,
-                        email,
-                        photoURL : downloadURL,
-                    });
-
-                    await setDoc(doc(db,"userChats",res.user.uid),{});
-
-                    
-                });
+  
+    const handleSubmit = async (e) => {
+      setLoading(true);
+      e.preventDefault();
+      const displayName = e.target[0].value;
+      const email = e.target[1].value;
+      const password = e.target[2].value;
+      const file = e.target[3].files[0];
+  
+      try {
+        //Create user
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        navigate("/");
+  
+        //Create a unique image name
+        const date = new Date().getTime();
+        const storageRef = ref(storage, `${displayName + date}`);
+  
+        await uploadBytesResumable(storageRef, file).then(() => {
+          getDownloadURL(storageRef).then(async (downloadURL) => {
+            try {
+              //Update profile
+              await updateProfile(res.user, {
+                displayName,
+                photoURL: downloadURL,
+              });
+              //create user on firestore
+              await setDoc(doc(db, "users", res.user.uid), {
+                uid: res.user.uid,
+                displayName,
+                email,
+                photoURL: downloadURL,
+              });
+  
+              //create empty user chats on firestore
+              await setDoc(doc(db, "userChats", res.user.uid), {});
+            } catch (err) {
+              console.log(err);
+              setErr(true);
+              setLoading(false);
             }
-            );
-            navigate("/");
-        }catch(err){
-            setErr(true)
-        }
-
-    }
-return (
-    <div className="form__container">
-        <div className="form__wrapper">
-            <span className="form__logo">Ihab Chat</span>
-            <span className="form__title">Register</span>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Display name" autoComplete="off" />
-                <input type="email" placeholder="email" autoComplete="off"/>
-                <input type="password" placeholder="password" autoComplete="off"/>
-                <input style={{display:"none"}} type="file" id="file"/>
-                <label htmlFor="file">
-                    <img src={addAvatar} alt="addAvatar" />
-                    <span>Add an Avatar</span>
-                </label>
-                <button>Sign up</button>
-                {err && <span>Something went wrong</span>}
-            </form>
-            <p>You do have an account? <Link to="/login">Login</Link></p>
+          });
+        });
+      } catch (err) {
+        setErr(true);
+        setLoading(false);
+      }
+    };
+    return (
+        <div className="form__container">
+            <div className="form__wrapper">
+                <span className="form__logo">Ihab Chat</span>
+                <span className="form__title">Register</span>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Display name" autoComplete="off" />
+                    <input type="email" placeholder="Email" autoComplete="off" />
+                    <input type="password" placeholder="Password" autoComplete="off" />
+                    <input style={{ display: "none" }} type="file" id="file" />
+                    <label htmlFor="file">
+                        <img src={addAvatar} alt="addAvatar" />
+                        <span>Add an Avatar</span>
+                    </label>
+                    <button>{loading ?(<div className="spinner"></div>):"Sign up"}</button>
+                    {err && <span>Something went wrong</span>}
+                </form>
+                <p>You already have an account? <Link to="/login">Login</Link></p>
+            </div>
         </div>
-    </div>
-)
-}
+    );
+};
 
-export default Register
+export default Register;
