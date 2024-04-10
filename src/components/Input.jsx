19bd205315c1db1df6ad08b59be9ddfbe6e4d97d
EHAB3x <ehabmah6969@gmail.com>
2024-04-10 23:51:23 +0200

    fix img issue #2

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Img from "../assets/img.png";
 import Attach from "../assets/attach.png";
 import { AuthContext } from "../context/AuthContext";
@@ -11,16 +11,15 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 const Input = () => {
   const [text, setText] = useState("");
   const [img, setImg] = useState(null);
-
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
 
   const handleSend = async () => {
     if (img) {
       const storageRef = ref(storage, uuid());
-
+            
       const uploadTask = uploadBytesResumable(storageRef, img);
-
+      
       uploadTask.on(
         () => {
           //TODO:Handle Error
@@ -49,21 +48,21 @@ const Input = () => {
         }),
       });
     }
-
+    
     await updateDoc(doc(db, "userChats", currentUser.uid), {
       [data.chatId + ".lastMessage"]: {
         text,
       },
       [data.chatId + ".date"]: serverTimestamp(),
     });
-
+    
     await updateDoc(doc(db, "userChats", data.user.uid), {
       [data.chatId + ".lastMessage"]: {
         text,
       },
       [data.chatId + ".date"]: serverTimestamp(),
     });
-
+    
     setText("");
     setImg(null);
   };
@@ -74,7 +73,7 @@ const Input = () => {
         placeholder="Type something..."
         onChange={(e) => setText(e.target.value)}
         value={text}
-      />
+        />
       <div className="send">
         <img src={Attach} alt="" />
         <input
