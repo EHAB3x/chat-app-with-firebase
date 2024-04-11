00c961190eba8c1109da1843d639f7fd97564157
EHAB3x <ehabmah6969@gmail.com>
2024-04-11 02:47:51 +0200

    change navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,11 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-      <span className="logo">Lama Chat</span>
       <div className="user">
-        <img src={currentUser.photoURL} alt="" />
-        <span>{currentUser.displayName}</span>
+        <div className='details'>
+          <img src={currentUser.photoURL} alt="" />
+          <span>{currentUser.displayName}</span>
+        </div>
         <button onClick={()=>signOut(auth)}>logout</button>
       </div>
     </div>
